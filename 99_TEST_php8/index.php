27ea68889294350e08438e80f6f7a4ba59ff83e2
Ivan Shevyrev <ivanvs140@yandex.ru>
2021-09-01 20:54:37 +0300

    test upd

--- 99_TEST_php8/index.php
+++ 99_TEST_php8/index.php
@@ -1,13 +1,17 @@
 <?php // phpcs:ignore PSR1.Files.SideEffects.FoundWithSymbols
 
 echo "<pre>";
+
 date_default_timezone_set('Europe/Moscow');
 setlocale(LC_ALL, 'ru_RU');
+
 $date_time = time();
-$date_time_format = strftime("%A %e %B %Y - %H:%M:%S", $date_time);
-echo "PHP version: " . PHP_VERSION . PHP_EOL;
-echo strftime("%A %e %B %Y - %H:%M:%S", $date_time) . PHP_EOL;
+$date_time_format = strftime("%A%e %B %Y - %H:%M:%S", $date_time);
+
+echo "PHP version: " . PHP_VERSION . PHP_EOL . $date_time_format . PHP_EOL;
 echo str_repeat("_", iconv_strlen($date_time_format)) . str_repeat(PHP_EOL, 2);
+
+define('TEXT_FILE', 'text-file.txt');
 
 // code here
 
