e237b96cf0c433c6b77b0d1de94208db87f96a6e
IvanVS140 <ivanvs140@yandex.ru>
2021-09-10 17:07:22 +0300

    date format fixed

--- 99_TEST_php8/index.php
+++ 99_TEST_php8/index.php
@@ -6,7 +6,7 @@ date_default_timezone_set('Europe/Moscow');
 setlocale(LC_ALL, 'ru_RU');
 
 $date_time = time();
-$date_time_format = strftime("%A%e %B %Y - %H:%M:%S", $date_time);
+$date_time_format = strftime("%A %e %B %Y - %H:%M:%S", $date_time);
 
 echo "PHP version: " . PHP_VERSION . PHP_EOL . $date_time_format . PHP_EOL;
 echo str_repeat("_", iconv_strlen($date_time_format)) . str_repeat(PHP_EOL, 2);
